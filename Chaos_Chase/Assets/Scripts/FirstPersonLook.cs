8905bdef2016ef0ed0f0e968a5b994e9b5d5378a
Ahin <ahinc0218@gmail.com>
2024-11-05 10:23:22 +0900

    Feat : 사건 일지 기록 기능 구현

--- Chaos_Chase/Assets/Scripts/FirstPersonLook.cs
+++ Chaos_Chase/Assets/Scripts/FirstPersonLook.cs
@@ -18,7 +18,7 @@ public class FirstPersonLook : MonoBehaviour
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
