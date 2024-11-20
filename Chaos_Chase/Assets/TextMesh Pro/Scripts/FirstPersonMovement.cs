7e9a82b307ac2a02df69274a78e7554f8b0c9525
ahinc218 <162945030+ahinc218@users.noreply.github.com>
2024-11-20 20:11:41 +0900

    Revert "Feature : 카메라 시야값 변경, 뛰기 코드 제거"

--- Chaos_Chase/Assets/TextMesh Pro/Scripts/FirstPersonMovement.cs
+++ Chaos_Chase/Assets/TextMesh Pro/Scripts/FirstPersonMovement.cs
@@ -5,8 +5,16 @@ public class FirstPersonMovement : MonoBehaviour
 {
     public float speed = 5;
 
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+    public KeyCode runningKey = KeyCode.LeftShift;
+
     Rigidbody playerRigidbody;
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+
 
     void Awake()
     {
@@ -15,13 +23,15 @@ public class FirstPersonMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        float targetMovingSpeed = speed;
+        IsRunning = canRun && Input.GetKey(runningKey);
+
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
         if (speedOverrides.Count > 0)
         {
             targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
         }
 
-        Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
 
         playerRigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, playerRigidbody.velocity.y, targetVelocity.y);
     }
