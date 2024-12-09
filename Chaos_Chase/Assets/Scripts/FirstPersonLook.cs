460a809365e75f2e649fa86e7eef00182dfc6242
Ahin <ahinc0218@gmail.com>
2024-12-09 23:51:55 +0900

    Feat : UI 활성화 시 카메라 및 플레이어 컨트롤 제한

--- Chaos_Chase/Assets/Scripts/FirstPersonLook.cs
+++ Chaos_Chase/Assets/Scripts/FirstPersonLook.cs
@@ -10,6 +10,10 @@ public class FirstPersonLook : MonoBehaviour
     Vector2 velocity;
     Vector2 frameVelocity;
 
+    public GameObject[] uiPanels;
+
+    private Quaternion originalCameraRotation;
+    private Quaternion originalCharacterRotation;
 
     void Reset()
     {
@@ -18,10 +22,27 @@ public class FirstPersonLook : MonoBehaviour
 
     void Start()
     {
-        //Cursor.lockState = CursorLockMode.Locked;
+        // 시작 시 카메라 및 캐릭터 초기 회전값 저장
+        originalCameraRotation = transform.localRotation;
+        originalCharacterRotation = character.localRotation;
     }
 
+    // UI 비활성화 시 마우스 조작에 따라 카메라 시야 변경, UI 활성화 시 회전값 고정되어 카메라 시야 고정
     void Update()
+    {
+        bool isUIActive = CheckUIActivation();
+
+        if (!isUIActive)
+        {
+            HandleCameraView();
+        }
+        else
+        {
+            LockCameraView();
+        }
+    }
+
+    private void HandleCameraView()
     {
         Vector2 mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         Vector2 rawFrameVelocity = Vector2.Scale(mouseDelta, Vector2.one * sensitivity);
@@ -31,5 +52,25 @@ public class FirstPersonLook : MonoBehaviour
 
         transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
         character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+    }
+
+    // 카메라 및 캐릭터 현재 회전값 고정
+    private void LockCameraView()
+    {
+        transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
+        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+    }
+
+    // UI가 활성화되어있는 확인하는 함수로 UI 패널 하나라도 활성화된 경우 true 반환
+    private bool CheckUIActivation()
+    {
+        foreach (GameObject panel in uiPanels)
+        {
+            if (panel.activeSelf)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- Chaos_Chase/Assets/Scripts/FirstPersonMovement.cs
+++ Chaos_Chase/Assets/Scripts/FirstPersonMovement.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class FirstPersonMovement : MonoBehaviour
 {
@@ -8,14 +9,28 @@ public class FirstPersonMovement : MonoBehaviour
     Rigidbody playerRigidbody;
     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
 
+    public GameObject[] uiPanels;
+
     void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
     }
 
+    // UI 비활성화 시 컨트롤 키에 따라 플레이어 이동, UI 활성화 시 이동 처리 중지
     void FixedUpdate()
     {
+        bool isUIActive = CheckUIActivation();
+
+        if (!isUIActive)
+        {
+            HandleControlKeys();
+        }
+    }
+
+    private void HandleControlKeys() 
+    {
         float targetMovingSpeed = speed;
+
         if (speedOverrides.Count > 0)
         {
             targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
@@ -24,5 +39,18 @@ public class FirstPersonMovement : MonoBehaviour
         Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
 
         playerRigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, playerRigidbody.velocity.y, targetVelocity.y);
+    }
+
+    // UI가 활성화되어있는 확인하는 함수로 UI 패널 하나라도 활성화된 경우 true 반환
+    private bool CheckUIActivation()
+    {
+        foreach (GameObject panel in uiPanels)
+        {
+            if (panel.activeSelf) 
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
